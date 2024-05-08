18c5042d7de027f70885c77725741a1f1e57e807
AsielELaouare <elaouareasiel82@gmail.com>
2024-05-08 19:54:40 +0200

    initial commit

---
+++ ChapeauModel/Chef.cs
@@ -0,0 +1,10 @@
+﻿namespace ChapeauModel
+{
+    public class Chef
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+    }
+}
---
+++ ChapeauService/ChefService.cs
@@ -0,0 +1,22 @@
+﻿using ChapeauDAL;
+using ChapeauModel;
+using System.Collections.Generic;
+
+namespace ChapeauService
+{
+    public class ChefService
+    {
+        private ChefDao chefdb;
+
+        public ChefService()
+        {
+            chefdb = new ChefDao();
+        }
+
+        public List<Chef> GetChefs()
+        {
+            List<Chef> chefs = chefdb.GetAllChefs();
+            return chefs;
+        }
+    }
+}
