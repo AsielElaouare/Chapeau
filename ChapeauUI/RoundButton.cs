2f118f53883898c05ca0df4a6e2732a9e9d86d3d
Toast3347 <706971@student.inholland.nl>
2024-05-20 13:35:59 +0200

    Login screen design
    The design for the login screen has been placed in the UI. This design is the same as the one in Figma at the time of upload.

---
+++ ChapeauUI/RoundButton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChapeauUI
+{
+    public class RoundButton : Button
+    {
+
+        public RoundButton()
+        {
+            this.FlatStyle = FlatStyle.Flat;   
+            this.FlatAppearance.BorderSize = 0;
+            this.BackColor = Color.FromArgb(0xD9, 0xD9, 0xD9);
+            this.Size = new Size(50, 50);
+        }
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            GraphicsPath graphics = new GraphicsPath();
+            graphics.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+            this.Region = new System.Drawing.Region(graphics);
+            base.OnPaint(pevent);
+        }
+
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            GraphicsPath graphics = new GraphicsPath();
+            graphics.AddEllipse(0, 0, this.Width, this.Height);
+            this.Region = new System.Drawing.Region(graphics);
+            Invalidate();  // Ensures the control is redrawn
+        }
+    }
+}
---
+++ ChapeauUI/RoundShape.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ChapeauUI
+{
+    public class RoundShape : UserControl
+    {
+        public RoundShape()
+        {
+            this.Size = new Size(50, 50);
+            this.BackColor = Color.FromArgb(0xD9, 0xD9, 0xD9);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            GraphicsPath graphics = new GraphicsPath();
+            graphics.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+            this.Region = new Region(graphics);
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            e.Graphics.Clear(this.Parent.BackColor);
+
+            using (SolidBrush brush = new SolidBrush(this.BackColor))
+            {
+                e.Graphics.FillEllipse(brush, 0, 0, this.ClientSize.Width, this.ClientSize.Height);
+            }
+
+            base.OnPaint(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            GraphicsPath graphics = new GraphicsPath();
+            graphics.AddEllipse(0, 0, this.Width, this.Height);
+            this.Region = new Region(graphics);
+            Invalidate();
+        }
+    }
+}
