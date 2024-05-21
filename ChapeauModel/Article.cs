819269c31373829ba1c1a7585194f3168db714a7
AsielELaouare <elaouareasiel82@gmail.com>
2024-05-21 22:28:50 +0200

    Removing chef model and adding article and order models and order status enum

---
+++ ChapeauModel/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapeauModel
+{
+    public class Article
+    {
+        public int articleId { get; set; }
+        public string name { get; set; }
+        public string category { get; set; } //this can be an enum
+        public int stock { get; set; }
+    }
+}
---
+++ ChapeauModel/Order.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace ChapeauModel
+{
+    public class Order
+    {
+        public List<Article> articles;
+        public int orderId { get; set; }
+        public OrderStatus status { get; set; }
+        public int numberOfArticles { get; set; }
+        public string comment { get; set; }
+        public int tableNr { get; set; }
+        public Order()
+        {
+            articles = new List<Article>();
+        }
+    }
+}
