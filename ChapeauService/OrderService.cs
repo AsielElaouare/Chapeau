2b380da30dd0c4fb6ebc778e47bac7a5692fd34b
AsielElaouare <elaouareasiel82@gmail.com>
2024-05-22 17:20:15 +0200

    creating orderService

---
+++ ChapeauService/OrderService.cs
@@ -0,0 +1,21 @@
+﻿using ChapeauDAL;
+using ChapeauModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapeauService
+{
+    public class OrderService
+    {
+
+        OrderDao orderDb = new OrderDao();
+        List<Order> orders = new List<Order>();
+        public void GetKitchenOrders()
+        {
+            orders = orderDb.GetAllOrders();
+        }
+    }
+}
